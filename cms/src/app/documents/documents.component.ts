a27ccefddb777cf86533b3aaf8872b2e92772a95
Daniel Emerson <91567321+eme20003@users.noreply.github.com>
2025-05-17 10:19:15 -0400

    Recent Adjustments to documents.component.ts

--- cms/src/app/documents/documents.component.ts
+++ cms/src/app/documents/documents.component.ts
@@ -8,8 +8,15 @@ import { Document } from './document.model';  // adjust path if needed
   styleUrls: ['./documents.component.css']
 })
 export class DocumentsComponent {
-  selectedDocument!: Document;
+  documents: Document[] = [
+    new Document('1', 'CIT 260', 'Object Oriented Programming', ''),
+    new Document('2', 'CIT 366', 'Full Web Stack Development', ''),
+    new Document('3', 'CIT 425', 'Data Warehousing', ''),
+    new Document('4', 'CIT 460', 'Enterprise Development', ''),
+    new Document('5', 'CIT 495', 'Senior Practicum', '')
+  ];
 
+  selectedDocument: Document | null = null; // Store the selected document
   // Handle event from DocumentListComponent
   onDocumentSelected(document: Document): void {
     this.selectedDocument = document;
